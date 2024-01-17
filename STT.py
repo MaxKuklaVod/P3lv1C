ce1aabdcb9be4d643e694dd7448a7a6429a93213
m513t20 <150217949+m513t20@users.noreply.github.com>
2024-01-17 22:31:32 +0800

    Теперь с whisper
    добавил whisper - функция STT_whisper, пример под ней

--- STT.py
+++ STT.py
@@ -9,20 +9,49 @@ from pathlib import Path
 # soundfile
 # SpeechRecognition
 # numpy
+#requests
+
+
+
+
+
+#whisper
+def STT_whisper(filename):
+    API_URL = "https://api-inference.huggingface.co/models/openai/whisper-large-v3"
+    headers = {"Authorization": "Bearer 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"}
+    with open(filename, "rb") as f:
+        data = f.read()
+    response = requests.post(API_URL, headers=headers, data=data)
+    return response.json()['text']
+#пример
+print(STT_whisper("audio.ogg"))
+
+
 
 
 def STT(path_to_file: str) -> str:
+    #Чтение оригинального файла
     data, samplerate = sf.read(path_to_file)
     out = "cur_STT.wav"
+    
+    #временный wav файл 
     sf.write(out, data, samplerate)
+    
+    #адресс временного файла
     adress = path.join(path.dirname(path.realpath(__file__)), out)
+    
+    #узнаватель текст
     rec = sr.Recognizer()
 
+
+    #аудио записывается через узнаватель
     with sr.AudioFile(adress) as source:
         # rec.adjust_for_ambient_noise(source)
         audio = rec.record(source)
         # cleaned_audio=rec.listen(source)
 
+
+    #вывод
     try:
         ret = rec.recognize_google(audio, language="ru-RU")
         print(ret)
@@ -39,3 +68,4 @@ def STT(path_to_file: str) -> str:
 
 
 STT('audio.ogg')
+
